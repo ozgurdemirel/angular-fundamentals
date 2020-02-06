7013e0757acf0ff8bdeffd0f523613814cb73ebd
ozgur.demirel <demirelozgur@gmail.com>
2020-02-06 12:33:14 +0100

    styling

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -5,7 +5,7 @@ import { EventEmitter } from '@angular/core';
 @Component({
     selector: '<event-thumbnail></event-thumbnail>',
     template: `
-        <div class="well test">
+        <div class="well">
             <h2>  {{event.name}} </h2>
             <div> {{event.date}}  </div>
             <div> {{event.price}}  </div>
@@ -14,12 +14,7 @@ import { EventEmitter } from '@angular/core';
                 <span> {{event.location.city}} - {{event.location.country}} </span>
             </div>
         </div>      
-    `,
-    styles:[`
-        .test {
-            background-color: white
-        }
-    `]
+    `
 })
 export class EventThumbnailComponent {
     @Input() event: any;
